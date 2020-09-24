0d6f13e4e27eda08056b8f0babdc632118102cfc
NHarendra <harendra@innolat.com>
2020-09-24 16:00:42 +0530

    password enc dec
    password enc dec

--- DemoCoreApi/Controllers/AuthorController.cs
+++ DemoCoreApi/Controllers/AuthorController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using DemoCoreApi.Models;
 using DemoCoreApi.RepositoryInterface;
@@ -29,11 +32,59 @@ namespace DemoCoreApi.Controllers
         [HttpPost]
         [Route("")]
         [AllowAnonymous]
-        public void AddAuthor([FromBody] Author author) => authorRepository.Insert(author);
+        public void AddAuthor([FromBody] Author author) {
+            author.Password = Encrypt(author.Password);
+            authorRepository.Insert(author); 
+        }
 
         [HttpDelete]
         [Route("{authorId}")]
         [AllowAnonymous]
         public void DeleteAuthor(Guid authorId) => authorRepository.Delete(authorId);
+
+        public static string Encrypt(string clearText)
+        {
+            string EncryptionKey = "HARENDRAKNISHAD1988";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "HARENDRAKNISHAD1988";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
     }
 }
